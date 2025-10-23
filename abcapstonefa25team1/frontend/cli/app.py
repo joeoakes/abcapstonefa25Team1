14a9a763b0442017e68bb32ce61ff8078ea3e9d9
Avik <avik812@gmail.com>
2025-10-23 09:12:59 -0400

    Added comment headers

--- abcapstonefa25team1/frontend/cli/app.py
+++ abcapstonefa25team1/frontend/cli/app.py
@@ -1,3 +1,11 @@
+# Project: TEAM 1 
+# Purpose Details: Set up CLI to test option inputs
+# Course: CMPSC488
+# Author: AVIK BHUIYAN
+# Date Developed: 10/18/2025
+# Last Date Changed: 10/23/2025
+# Revision: Setting up CLI structure for testing
+
 import argparse
 from testMethods import greet
 # Define a simple function with an argument (PLACEHOLDER UNTIL CODE IS READY)
@@ -5,13 +13,13 @@ from testMethods import greet
 # Set up a command-line interface
 def main():
     parser = argparse.ArgumentParser(description="A simple greeting tool")
-    parser.add_argument("--name", "-n", required=True, help="Your name")
+    parser.add_argument("--file", "-n", required=True)
 
     # Parse the command-line arguments
     args = parser.parse_args()
 
     # Call the function with the command-line value
-    greet(args.name)
+    greet(args.file)
 
 if __name__ == "__main__":
     main()
