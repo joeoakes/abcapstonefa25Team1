5035110626b877a2a197649826986c42ab595ba9
Kamila-An <kfa5397@psu.edu>
2025-10-30 09:56:53 -0400

    Added docstring comments to benchmarking.py

--- abcapstonefa25team1/backend/utils/benchmarking.py
+++ abcapstonefa25team1/backend/utils/benchmarking.py
@@ -15,8 +15,15 @@ import timeit
 
 def benchmark_function(func, *args, **kwargs):
     """
-    Measures execution time for a dingle function call.
-    Returns elapsed time in seconds.
+    Measures execution time for a single function call.
+
+    Args:
+    func (callable): The function to benchmark.
+    *args: Positional arguments to pass to the function.
+    **kwargs: Keyword arguments to pass to the function.
+
+    Returns:
+    float: Elapsed time in seconds, rounded to six decimal places.
     """
     start = time.time()
     func(*args, **kwargs)             # Execute the target function with its arguments
@@ -27,7 +34,16 @@ def benchmark_function(func, *args, **kwargs):
 def average_benchmark(stmt, setup, number = 10):
     """
     Benchmark using timeit — runs code multiple times and returns average time.
+    
+    Args:
+    stmt (str): Code to execute
+    setup (str): Setup code executed before running the benchmark.
+    number(int, optional): Number of times to run. Defaults to 10.
+
+    Returns:
+    float: Average execution time per run in seconds, rounded to six decimal places.
     """
     total_time = timeit.timeit(stmt = stmt, setup = setup, number = number)
     avg_time = total_time /number
     return round(avg_time, 6)
+
